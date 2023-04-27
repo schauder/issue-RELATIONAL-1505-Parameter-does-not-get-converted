fff1578333784924082f7d6f050dd6140a2622bc
Salathiel <salathiel@genese.name>
2023-04-27 20:18:15 +0200

    init

---
+++ src/main/java/name/genese/salathiel/r2dbcuuidconverters/R2dbcUuidConvertersApplication.java
@@ -0,0 +1,13 @@
+package name.genese.salathiel.r2dbcuuidconverters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcUuidConvertersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcUuidConvertersApplication.class, args);
+	}
+
+}
